aceaa9fc7c6e7c96a5c140308a22404bb4c56f3f
Fendy Saputro <fendy@amg.co.id>
2020-07-01 08:40:23 +0700

    update

--- app/src/main/java/com/stuff/stuffshare/activity/SplashScreenActivity.java
+++ app/src/main/java/com/stuff/stuffshare/activity/SplashScreenActivity.java
@@ -5,6 +5,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +18,29 @@ import com.stuff.stuffshare.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView logo, slogan;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splashscreen);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        topAnim.setDuration(2000);
+        bottomAnim.setDuration(1500);
+
+        imageView = (ImageView) findViewById(R.id.iconMms);
+        logo = (TextView) findViewById(R.id.logoText);
+        slogan = (TextView) findViewById(R.id.sloganText);
+
+        imageView.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
