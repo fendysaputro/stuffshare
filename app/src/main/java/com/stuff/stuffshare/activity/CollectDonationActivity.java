2df2a4aea2d352e588f93381168f1fe411c2f321
phephen <vidis194@gmail.com>
2021-01-27 00:16:05 +0700

    update ui collect_donation_activity.xml

--- app/src/main/java/com/stuff/stuffshare/activity/CollectDonationActivity.java
+++ app/src/main/java/com/stuff/stuffshare/activity/CollectDonationActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
